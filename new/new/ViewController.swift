6f682fceba8c7fafa3db9993ba58acb98ee3c228
Natalia Grigoreva <ms.pritchina@gmail.com>
2023-02-28 18:19:56 +0300

    initial

---
+++ new/new/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  new
+//
+//  Created by Nataly on 28.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("hi")
+    }
+
+
+}
+
