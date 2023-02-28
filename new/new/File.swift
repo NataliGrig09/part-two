804abae4594f86325adb760bf2a8caab06bc4ddf
Natalia Grigoreva <ms.pritchina@gmail.com>
2023-02-28 18:50:02 +0300

    added name

---
+++ new/new/File.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  new
+//
+//  Created by Nataly on 28.02.2023.
+//
+
+import UIKit
+
+class File: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Nata")
+    }
+
+
+}
